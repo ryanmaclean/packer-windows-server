8d4a9b5bb57ca2efe289ec5412c37f486280714f
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2021-12-07 18:39:49 -0800

    Update install-chocolatey.ps1

--- scripts/install-chocolatey.ps1
+++ scripts/install-chocolatey.ps1
@@ -1,4 +1,4 @@
 # ./scripts/chocolatey.ps1
 # Install Chocolatey
 Write-Output "Installing Chocolatey"
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
