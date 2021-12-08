bef8b8dab2dcaaa1a80c6683ba7566e23ac44d29
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2021-12-07 18:40:49 -0800

    Update install-openssh.ps1

--- scripts/install-openssh.ps1
+++ scripts/install-openssh.ps1
@@ -10,7 +10,7 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 # Get-NetFirewallRule -Name *ssh*
 # There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled
 # If the firewall does not exist, create one
-# New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
